83a09ffb3d3f69e19e0bf1f8307b2fdc8efd60ab
Nukebommer360 <trevorirena@gmail.com>
2026-03-27 16:37:17 -0700

    Changed how you step motor.

--- src/busclock/electronics/motor.py
+++ src/busclock/electronics/motor.py
@@ -1,13 +1,9 @@
-from filecmp import dircmp
 import time 
 import RPi.GPIO as GPIO
 
 class MotorControl:
-    secondsPerStep = 0
-    currentShownTime = 0
     # in1, in2, in3, in4
     pins = [18, 23, 24, 25]
-    step_count = 4096
     step_sequence = [[1,0,0,1],
                  [1,0,0,0],
                  [1,1,0,0],
@@ -16,7 +12,6 @@ class MotorControl:
                  [0,0,1,0],
                  [0,0,1,1],
                  [0,0,0,1]]
-
     motor_step_counter = 0
 
     def __init__(self):
@@ -31,19 +26,13 @@ class MotorControl:
         GPIO.output(self.pins[2], GPIO.LOW)
         GPIO.output(self.pins[3], GPIO.LOW)
 
-    def step(self, dir):
-        if (dir):
-            self.motor_step_counter = (self.motor_step_counter + 1) % 8 
-        else:
-            self.motor_step_counter = (self.motor_step_counter - 1) % 8
-        
-        for pin in range(0, len(self.pins)):
-            GPIO.output(self.pins[pin], self.step_sequence[self.motor_step_counter][pin] )
-
-    def setDisplayedTime(self, time):
-        pass
-    def addTime(self):
-        pass
+    def step(self, steps):
+        step = -2 if steps < 0 else 2
+        for i in range(abs(steps)):
+            self.motor_step_counter = self.motor_step_counter + step
+            for pin in range(0, len(self.pins)):
+                GPIO.output(self.pins[pin], self.step_sequence[self.motor_step_counter % 8][pin])
+            time.sleep(0.002)
 
     def cleanup(self):
         GPIO.output(self.pins[0], GPIO.LOW)
@@ -54,14 +43,12 @@ class MotorControl:
 
 if __name__ == "__main__":
     motor = MotorControl()
-    dir = 0
+    steps = 2048
     try:
         while(1):
-            for i in range(0, 4096):
-                motor.step(dir)
-                time.sleep(0.002)
-            time.sleep(5)
-            dir = ~dir
+            motor.step(steps)
+            time.sleep(1)
+            steps *= -1
     except KeyboardInterrupt:
         motor.cleanup()
         exit(1)
