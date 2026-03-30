85e50591af9c79e610ab4fc9ba555a815f10a278
Meela Parker <meelaparker9@gmail.com>
2026-03-30 13:38:30 -0700

    fixed LEDs to mapped values

--- src/busclock/LED_management/rain_frame.py
+++ src/busclock/LED_management/rain_frame.py
@@ -9,88 +9,48 @@ from display import display
             frame[3]=RGB(0,0,100)
             frame[7]=RGB(0,0,100)
             frame[11]=RGB(0,0,100)
-            frame[15]=RGB(0,0,100)
-            frame[23]=RGB(0,0,100)
+            frame[18]=RGB(0,0,100)
+            frame[22]=RGB(0,0,100)
+            frame[30]=RGB(0,0,100)
             return frame
     
         elif step ==2:
             frame = blank_frame()
 
-            frame[5]=RGB(0,0,100)
-
+            frame[19]=RGB(0,0,100)
+            frame[29]=RGB(0,0,100)
+            frame[54]=RGB(0,0,100)
             frame[40]=RGB(0,0,100)
-            frame[31]=RGB(0,0,100)
-            frame[60]=RGB(0,0,100)
-            frame[49]=RGB(0,0,100)
-            frame[45]=RGB(0,0,100)
+            frame[27]=RGB(0,0,100)
+            frame[69]=RGB(0,0,100)
             return frame
     
         elif step ==3:
             frame = blank_frame()
-            frame[29]=RGB(0,0,100)
-            frame[1]=RGB(0,0,100)
-            frame[14]=RGB(0,0,100)
-            frame[22]=RGB(0,0,100)
-            frame[25]=RGB(0,0,100)
-
-            frame[57]=RGB(0,0,100)
-            frame[70]=RGB(0,0,100)
-            frame[97]=RGB(0,0,100)
-            frame[65]=RGB(0,0,100)
-            frame[87]=RGB(0,0,100)
+            frame[42]=RGB(0,0,100)
+            frame[71]=RGB(0,0,100)
+            frame[95]=RGB(0,0,100)
+            frame[55]=RGB(0,0,100)
+            frame[49]=RGB(0,0,100)
+            frame[83]=RGB(0,0,100)
             return frame
         elif step ==4:
             frame = blank_frame()
-            frame[68]=RGB(0,0,100)
-            frame[39]=RGB(0,0,100)
-            frame[60]=RGB(0,0,100)
-            frame[44]=RGB(0,0,100)
-            frame[48]=RGB(0,0,100)
-
+            frame[51]=RGB(0,0,100)
             frame[81]=RGB(0,0,100)
-            frame[84]=RGB(0,0,100)
-            frame[77]=RGB(0,0,100)
-            frame[101]=RGB(0,0,100)
+            frame[94]=RGB(0,0,100)
+            frame[73]=RGB(0,0,100)
+            frame[98]=RGB(0,0,100)
             return frame
         elif step ==5:
             frame = blank_frame()
-            frame[3]=RGB(0,0,100)
-            frame[7]=RGB(0,0,100)
-            frame[11]=RGB(0,0,100)
-            frame[15]=RGB(0,0,100)
-            frame[23]=RGB(0,0,100)
-
-            frame[82]=RGB(0,0,100)
-            frame[56]=RGB(0,0,100)
-            frame[97]=RGB(0,0,100)
-            frame[63]=RGB(0,0,100)
-            frame[65]=RGB(0,0,100)
-
-            frame[92]=RGB(0,0,100)
-            frame[103]=RGB(0,0,100)
-        elif step ==6:
-            frame=blank_frame()
-            frame[40]=RGB(0,0,100)
-            frame[31]=RGB(0,0,100)
-            frame[60]=RGB(0,0,100)
-            frame[49]=RGB(0,0,100)
-            frame[45]=RGB(0,0,100)
-
-            frame[102]=RGB(0,0,100)
             frame[80]=RGB(0,0,100)
             frame[100]=RGB(0,0,100)
-            return frame
-        elif step ==7:
-            frame=blank_frame()
-            frame[57]=RGB(0,0,100)
-            frame[70]=RGB(0,0,100)
-            frame[97]=RGB(0,0,100)
-            frame[66]=RGB(0,0,100)
-            frame[87]=RGB(0,0,100)
+            frame[76]=RGB(0,0,100)
 #rain animation
     def rain_animation(self):
         while True:
-            for(step) in range(1,7):
+            for(step) in range(1,5):
                 frame = self.rain_frame(step)
                 display(frame)
                 time.sleep(0.2)
--- src/busclock/LED_management/snowing.py
+++ src/busclock/LED_management/snowing.py
@@ -15,84 +15,52 @@ class SnowAnimation:
             frame[22]=RGB(100,100,100)
             frame[18]=RGB(100,100,100)
             frame[25]=RGB(100,100,100)
+            frame[29]=RGB(100,100,100)
             return frame
         
         elif step ==2:
             frame = blank_frame()
 
-            frame[30]=RGB(100,100,100)
-            frame[19]=RGB(100,100,100)
+            frame[28]=RGB(100,100,100)
             frame[20]=RGB(100,100,100)
-            frame[43]=RGB(100,100,100)
-            frame[39]=RGB(100,100,100)
-            frame[48]=RGB(100,100,100)
+            frame[40]=RGB(100,100,100)
+            frame[44]=RGB(100,100,100)
+            frame[38]=RGB(100,100,100)
+            frame[46]=RGB(100,100,100)
+            frame[69]=RGB(100,100,100)
             return frame
         
         elif step ==3:
             frame = blank_frame()
-            frame[8]=RGB(100,100,100)
-            frame[14]=RGB(100,100,100)
-            frame[3]=RGB(100,100,100)
-            frame[17]=RGB(100,100,100)
-            frame[23]=RGB(100,100,100)
-        
-
-
-            frame[70]=RGB(100,100,100)
-            frame[41]=RGB(100,100,100)
-            frame[45]=RGB(100,100,100)
-            frame[62]=RGB(100,100,100)
-            frame[57]=RGB(100,100,100)
+            frame[53]=RGB(100,100,100)
+            frame[42]=RGB(100,100,100)
+            frame[55]=RGB(100,100,100)
+            frame[73]=RGB(100,100,100)
             frame[65]=RGB(100,100,100)
+            frame[75]=RGB(100,100,100)
+            frame[81]=RGB(100,100,100)
             return frame
         elif step ==4:
             frame = blank_frame()
-            frame[32]=RGB(100,100,100)
-            frame[42]=RGB(100,100,100)
-            frame[40]=RGB(100,100,100)
+            frame[79]=RGB(100,100,100)
             frame[50]=RGB(100,100,100)
-            frame[44]=RGB(100,100,100)
-
-            frame[84]=RGB(100,100,100)
-            frame[59]=RGB(100,100,100)
-            frame[63]=RGB(100,100,100)
+            frame[94]=RGB(100,100,100)
+            frame[76]=RGB(100,100,100)
             frame[87]=RGB(100,100,100)
-            frame[80]=RGB(100,100,100)
-            frame[78]=RGB(100,100,100)
+            frame[99]=RGB(100,100,100)
             return frame
         elif step ==5:
             frame = blank_frame()
-            frame[71]=RGB(100,100,100)
-            frame[62]=RGB(100,100,100)
-            frame[57]=RGB(100,100,100)
-            frame[66]=RGB(100,100,100)
-            frame[88]=RGB(100,100,100)
-
-            frame[104]=RGB(100,100,100)
-            frame[59]=RGB(100,100,100)
-            frame[63]=RGB(100,100,100)
-            frame[87]=RGB(100,100,100)
+            frame[106]=RGB(100,100,100)
             frame[80]=RGB(100,100,100)
-            frame[78]=RGB(100,100,100)
+            frame[93]=RGB(100,100,100)
 
             return frame
-        elif step ==6:
-            frame = blank_frame()
-            frame[85]=RGB(100,100,100)
-            frame[87]=RGB(100,100,100)
-            frame[81]=RGB(100,100,100)
-            frame[77]=RGB(100,100,100)
-            return frame
-        elif step==7:
-            frame = blank_frame()
-            frame[105]=RGB(100,100,100)
-            frame[101]=RGB(100,100,100)
-            frame[92]=RGB(100,100,100)
-            return frame
+       
 #snow animation 
     def snow_animation(self):
         while True:
-            for(step) in range(1,7):
+            for(step) in range(1,5):
                 frame = self.snow_frame(step)
                 self.leds.display(frame)
                 time.sleep(0.5)
